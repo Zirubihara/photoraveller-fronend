99c25e258d74f4a8d270ba78906739c4802229f6
Krystian <krystianw.e.t@gmail.com>
2020-07-24 00:31:56 +0200

    working on sign up

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {SignUpRequestPayload} from './SignUpRequestPayload';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,14 +9,20 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
 
+  signUpRequestPayload: SignUpRequestPayload;
   signupForm: FormGroup;
 
   constructor() {
-    this.signupForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required)
-    });
+    // this.signupForm = new FormGroup({
+    //   username: new FormControl('', Validators.required),
+    //   email: new FormControl('', [Validators.required, Validators.email]),
+    //   password: new FormControl('', Validators.required)
+    // });
+    this.signUpRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
   }
 
   ngOnInit(): any {
@@ -24,6 +31,13 @@ export class SignUpComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
     });
+  }
+
+  signup(): any {
+    this.signUpRequestPayload.username = this.signupForm.get('username').value;
+    this.signUpRequestPayload.email = this.signupForm.get('email').value;
+    this.signUpRequestPayload.password = this.signupForm.get('password').value;
+
   }
 
 }
